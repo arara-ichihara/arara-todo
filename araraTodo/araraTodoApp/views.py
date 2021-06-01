07b312e57f3eb937e53eeb98d7d6a462b5f07da1
arara-trongtuan <84692372+arara-trongtuan@users.noreply.github.com>
2021-06-01 13:46:08 +0900

    add addTodo function

--- araraTodo/araraTodoApp/views.py
+++ araraTodo/araraTodoApp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import TodoItem
+from django.utils import timezone
 
 def index(request):
     todo_list = TodoItem.objects.all()
     context = {'todo_list':todo_list}
     return render(request, 'araraTodoApp/index.html', context)
+
+def addTodo(request):
+    new = request.POST['content']
+    new_todo_item = TodoItem(content=new, create_date=timezone.now())
+    new_todo_item.save()
+    return HttpResponseRedirect('/araraTodoApp/')
