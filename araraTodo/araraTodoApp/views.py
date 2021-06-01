caff63c2f7953d4df2cc987f3ba4f4d50a48626c
arara-trongtuan <84692372+arara-trongtuan@users.noreply.github.com>
2021-06-01 10:58:37 +0900

    show TodoItem in index.html

--- araraTodo/araraTodoApp/views.py
+++ araraTodo/araraTodoApp/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import TodoItem
 
 def index(request):
-    return render(request, 'araraTodoApp/index.html')
+    todo_list = TodoItem.objects.all()
+    context = {'todo_list':todo_list}
+    return render(request, 'araraTodoApp/index.html', context)
