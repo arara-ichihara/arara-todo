b2351615a16a257c6ac927738c2d52bdbd5ec9af
arara-ogawa-s <ogawa.s@arara.com>
2021-06-04 17:58:18 +0900

    update todo

--- araraTodoApp/urls.py
+++ araraTodoApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/delete/', views.delete, name='delete'),  # ←削除機能用
+    path('<int:id>/update/', views.update, name='update'),  # ←削除機能用
 ]
